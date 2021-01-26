bd3d3575a0e87acf98b3d94dfe198365b962ea17
long1712578 <pdlong578@gmail.com>
2021-01-26 07:43:23 +0700

    first commit

---
+++ Receive/Receive.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receive
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() {
+                Uri=new Uri("amqp://guest:guest@localhost:5762")
+             };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "demo-queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = new {nameof="product", message="Hello!"};
+                var body=Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                channel.BasicPublish("","demo-queue",null,body);
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Send
+{
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() {
+                Uri=new Uri("amqp://guest:guest@localhost:5762")
+             };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "demo-queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer=new EventingBasicConsumer(channel);
+                consumer.Received+=(sender,e)=>{
+                    var body=e.Body.ToArray();
+                    var message=Encoding.UTF8.GetString(body);
+                    console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
